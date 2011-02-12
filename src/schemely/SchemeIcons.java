9db01a52943fa96140b2dd110f7613a72b0416b3
Colin Fleming <colin@colinfleming.net>
2011-02-13 00:56:33 +0100

    Move Icons to correct package

--- src/schemely/SchemeIcons.java
+++ src/schemely/SchemeIcons.java
@@ -8,12 +8,12 @@ import javax.swing.*;
 public interface SchemeIcons
 {
   @NonNls
-  final Icon SCHEME_ICON = IconLoader.findIcon("/schemely/icons/scheme_icon_16x16.png");
+  final Icon SCHEME_ICON = IconLoader.findIcon("/schemely/icons/lambda.png");
 
-  final Icon FUNCTION = IconLoader.findIcon("/schemely/icons/def_tmp.png");
-  final Icon METHOD = IconLoader.findIcon("/schemely/icons/meth_tmp.png");
-  final Icon JAVA_METHOD = IconLoader.findIcon("/schemely/icons/method.png");
-  final Icon JAVA_FIELD = IconLoader.findIcon("/schemely/icons/field.png");
+  final Icon FUNCTION = IconLoader.findIcon("/schemely/icons/function.png");
+  final Icon METHOD = IconLoader.findIcon("/schemely/icons/method.png");
+  final Icon JAVA_METHOD = IconLoader.findIcon("/schemely/icons/java_method.png");
+  final Icon JAVA_FIELD = IconLoader.findIcon("/schemely/icons/java_field.png");
   final Icon SYMBOL = IconLoader.findIcon("/schemely/icons/symbol.png");
   final Icon NAMESPACE = IconLoader.findIcon("/schemely/icons/namespace.png");
 }
