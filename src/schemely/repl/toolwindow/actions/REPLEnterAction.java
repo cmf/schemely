2e3a5d9da8191cb07d4b8d16bd8151b467639e67
Colin Fleming <colin@colinfleming.net>
2011-03-10 10:12:35 +0100

    Fixed auto-indentation in the REPL when the user presses enter. Closes #16.

--- src/schemely/repl/toolwindow/actions/REPLEnterAction.java
+++ src/schemely/repl/toolwindow/actions/REPLEnterAction.java
@@ -2,8 +2,6 @@ package schemely.repl.toolwindow.actions;
 
 import com.intellij.execution.process.ConsoleHistoryModel;
 import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.application.Result;
-import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -13,9 +11,6 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.impl.source.codeStyle.HelperFactory;
-import com.intellij.psi.impl.source.codeStyle.IndentHelper;
-import schemely.file.SchemeFileType;
 import schemely.psi.util.SchemePsiUtil;
 import schemely.repl.SchemeConsole;
 import schemely.repl.actions.NewSchemeConsoleAction;
@@ -28,7 +23,6 @@ import schemely.utils.Editors;
 public class REPLEnterAction extends EditorWriteActionHandler implements DumbAware
 {
   private final EditorActionHandler originalHandler;
-  protected IndentHelper indentHelper = null;
 
   public REPLEnterAction(EditorActionHandler originalHandler)
   {
@@ -43,49 +37,38 @@ public class REPLEnterAction extends EditorWriteActionHandler implements DumbAwa
     {
       originalHandler.execute(editor, dataContext);
     }
-    else
+    else if (!runExecuteAction(repl, false))
     {
-      runExecuteAction(repl, false);
+      originalHandler.execute(editor, dataContext);
     }
   }
 
-  protected void runExecuteAction(REPL repl, boolean executeImmediately)
+  @Override
+  public boolean isEnabled(Editor editor, DataContext dataContext)
   {
-    final SchemeConsole console = repl.getConsoleView().getConsole();
+    return originalHandler.isEnabled(editor, dataContext);
+  }
+
+  protected boolean runExecuteAction(REPL repl, boolean executeImmediately)
+  {
+    SchemeConsole console = repl.getConsoleView().getConsole();
     if (executeImmediately)
     {
       execute(repl);
-      return;
+      return true;
     }
 
     Project project = console.getProject();
-    indentHelper = HelperFactory.createHelper(SchemeFileType.SCHEME_FILE_TYPE, project);
 
     Editor editor = console.getCurrentEditor();
     Document document = editor.getDocument();
-    final CaretModel caretModel = editor.getCaretModel();
-    final int offset = caretModel.getOffset();
+    CaretModel caretModel = editor.getCaretModel();
+    int offset = caretModel.getOffset();
     String text = document.getText();
 
     if (!"".equals(text.substring(offset).trim()))
     {
-      String before = text.substring(0, offset);
-      String after = text.substring(offset);
-      final int indent = indentHelper.getIndent(before, false);
-      String spaces = indentHelper.fillIndent(indent);
-      final String newText = before + '\n' + spaces + after;
-
-      new WriteCommandAction(project)
-      {
-        @Override
-        protected void run(Result result) throws Throwable
-        {
-          console.setInputText(newText);
-          caretModel.moveToOffset(offset + indent + 1);
-        }
-      }.execute();
-
-      return;
+      return false;
     }
 
     String candidate = text.trim();
@@ -94,29 +77,15 @@ public class REPLEnterAction extends EditorWriteActionHandler implements DumbAwa
     {
       execute(repl);
       Editors.scrollDown(editor);
-    }
-    else
-    {
-      int indent = indentHelper.getIndent(text, false);
-      String spaces = indentHelper.fillIndent(indent);
-      String newText = text + '\n' + spaces + spaces(console.getPrompt().length());
-      console.setInputText(newText);
-    }
-  }
-
-  private String spaces(int length)
-  {
-    StringBuilder builder = new StringBuilder(length);
-    for (int i = 0; i < length; i++)
-    {
-      builder.append(' ');
+      return true;
     }
 
-    return builder.toString();
+    return false;
   }
 
   private void execute(REPL repl)
   {
+    // TODO check scrolling, unify REPL execution in single place
     SchemeConsole console = repl.getConsoleView().getConsole();
     ConsoleHistoryModel consoleHistoryModel = console.getHistoryModel();
 
