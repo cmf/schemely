9d97b0e8d7fe1e978f466257c1e74355a6b4e186
Colin Fleming <colin@colinfleming.net>
2011-03-22 09:02:00 +0100

    Disable Kawa for now so I can concentrate on SISC.

--- src/schemely/scheme/SchemeImplementation.java
+++ src/schemely/scheme/SchemeImplementation.java
@@ -10,7 +10,7 @@ import schemely.settings.SchemeProjectSettings;
  */
 public enum SchemeImplementation
 {
-  KAWA_1_11("Bundled Kawa 1.11", new KawaScheme()),
+//  KAWA_1_11("Bundled Kawa 1.11", new KawaScheme()),
   SISC_1_16_6("Bundled SISC 1.16.6", new SISCScheme());
 
   private final String description;
