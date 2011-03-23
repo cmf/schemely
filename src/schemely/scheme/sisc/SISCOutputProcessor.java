6b60da517a7e56ea1ea4ccf4201963cdfbb46230
Colin Fleming <colin@colinfleming.net>
2011-03-23 09:59:59 +0100

    Console error output is now styled as such.

--- src/schemely/scheme/sisc/SISCOutputProcessor.java
+++ src/schemely/scheme/sisc/SISCOutputProcessor.java
@@ -3,11 +3,16 @@ package schemely.scheme.sisc;
 import com.intellij.execution.console.LanguageConsoleImpl;
 import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.util.text.StringUtil;
+import dk.brics.automaton.Automaton;
+import dk.brics.automaton.BasicOperations;
+import dk.brics.automaton.RegExp;
+import dk.brics.automaton.RunAutomaton;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NonNls;
 import schemely.repl.SchemeConsole;
 import schemely.utils.Editors;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,7 +21,12 @@ import java.util.regex.Pattern;
  */
 public class SISCOutputProcessor
 {
-  enum State { STARTING, EXECUTING }
+  private static final RunAutomaton ERRORS = errors();
+
+  enum State
+  {
+    STARTING, EXECUTING
+  }
 
   private State state = State.STARTING;
 
@@ -79,8 +89,38 @@ public class SISCOutputProcessor
 
   public void flush()
   {
-    LanguageConsoleImpl.printToConsole(console, buffer.toString(), ConsoleViewContentType.NORMAL_OUTPUT, null);
+    String output = buffer.toString();
+    ConsoleViewContentType style = ConsoleViewContentType.NORMAL_OUTPUT;
+    if (looksLikeError(output))
+    {
+      style = ConsoleViewContentType.ERROR_OUTPUT;
+    }
+    LanguageConsoleImpl.printToConsole(console, output, style, null);
     Editors.scrollDown(console.getHistoryViewer());
     buffer.setLength(0);
+  }
+
+  static boolean looksLikeError(String output)
+  {
+    int length = ERRORS.run(output, 0);
+    return length > 0;
+  }
+
+  private static RunAutomaton errors()
+  {
+    Automaton errors = BasicOperations.union(Arrays.asList(automaton("Error\\.[ \t]*\n"),
+                                                           automaton("Error in .+\\.[ \t]*\n"),
+                                                           automaton("Error:[^\n]+\n"),
+                                                           automaton("Error in .+: [^\n]+\n"),
+                                                           automaton("Error in nested call\\.[ \t]*\n"),
+                                                           automaton("Error in nested call: [^\n]+\n"),
+                                                           automaton("Error in nested call from .+\\.[ \t]*\n"),
+                                                           automaton("Error in nested call from .+: [^\n]+\n")));
+    return new RunAutomaton(errors);
+  }
+
+  private static Automaton automaton(String regexp)
+  {
+    return new RegExp(regexp).toAutomaton();
   }
 }
